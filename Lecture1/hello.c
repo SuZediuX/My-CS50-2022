37caae358ea650448aeee3d81241b040cb340051
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-20 22:05:58 +0100

    Added user intake and output

--- Lecture1/hello.c
+++ Lecture1/hello.c
@@ -2,5 +2,11 @@
 
 int main(void)
 {
-    printf("Hello, world! \n");
+    //stdio answer = get_string("What's your name?");
+    char name[30];
+    printf("Enter name: \n");
+    fgets(name, sizeof(name), stdin);
+    printf("Name: ");
+    puts(name);
+    return 0;
 }
