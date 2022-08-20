a967fd395b156d7838931be747339f8608c6c7c7
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-20 22:18:48 +0100

    Improved output format

--- Lecture1/hello.c
+++ Lecture1/hello.c
@@ -2,11 +2,10 @@
 
 int main(void)
 {
-    //stdio answer = get_string("What's your name?");
     char name[30];
     printf("Enter name: \n");
     fgets(name, sizeof(name), stdin);
-    printf("Name: ");
-    puts(name);
+    printf("Name: %s\n", name);
+    //puts(name);
     return 0;
 }
