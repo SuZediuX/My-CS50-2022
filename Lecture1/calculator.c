7e4919d9486bb8850ec7dfdff5f7508e2d8444cb
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-20 23:44:15 +0100

    Improved output format

--- Lecture1/calculator.c
+++ Lecture1/calculator.c
@@ -2,9 +2,15 @@
 
 int main(void)
 {
-    int x;
+    int x, y;
     printf("Enter a value for x: ");
     scanf("%d", &x);
-    printf("You entered, for x: %d", x);
+    printf("You entered, for x: %d \n", x);
+
+    printf("Enter a value for y: ");
+    scanf("%d", &y);
+    printf("You entered, for y: %d \n", y);
+
+    printf("Sum of x and y is: %i \n", x+y);
     return 0;
 }
