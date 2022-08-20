4d3fd677a9571185aa316c7c10a7b6251cb9ba67
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-20 23:58:39 +0100

    Improved var names

--- Lecture1/calculator.c
+++ Lecture1/calculator.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-    int x, y;
+    int first_num, second_num;
     printf("Enter a value for x: ");
-    scanf("%d", &x);
-    printf("You entered, for x: %d \n", x);
+    scanf("%d", &first_num);
+    printf("You entered, for x: %d \n", first_num);
 
     printf("Enter a value for y: ");
-    scanf("%d", &y);
-    printf("You entered, for y: %d \n", y);
+    scanf("%d", &second_num);
+    printf("You entered, for y: %d \n", second_num);
 
-    printf("Sum of x and y is: %i \n", x+y);
+    printf("Sum of x and y is: %i \n", first_num+second_num);
     return 0;
 }
