7bdb47541db82ca75d5c6a8804be3f51582bceb1
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-21 20:33:40 +0100

    Add new file

---
+++ Lecture1/points.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int points;
+    int my_points = 2;
+    
+    printf("How many points did you lose?\n");
+    scanf("%i", &points);
+
+    if(points < my_points)
+    {
+        printf("You lost less points than me.\n");
+    }
+    else if(points > my_points)
+    {
+        printf("You lost more points than me.\n")
+    }
+    else
+    {
+        printf("You lost the same number of points as me");
+    }
+    return 0;
+}
