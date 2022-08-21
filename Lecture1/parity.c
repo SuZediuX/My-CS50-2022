01c7ba80ae71705c5bb677c141167df3b01f7617
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-21 20:40:53 +0100

    Add new file

---
+++ Lecture1/parity.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    
+    printf("Enter an integer: \n");
+    scanf("%i", &n);
+
+    if(n%2==0)
+    {
+        printf("The number you entered is even!\n");
+    }
+    else
+    {
+        printf("The number you entered is odd!\n");
+    }
+
+    return 0;
+}
