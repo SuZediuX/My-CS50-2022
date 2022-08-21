b4ca969ec88e680850f829dfb6f61db0dd95e1ca
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-21 20:56:14 +0100

    Add unstaged file

---
+++ Lecture1/agree.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char store;
+    printf("Do you agree?\n");
+    scanf("%s", &store);
+
+    if(store=='y'||store=='Y')
+    {
+        printf("Agreed.\n");
+    }
+    else if(store=='n'||store=='N')
+    {
+        printf("Didn't agree.\n");
+    }
+
+    return 0;
+}
