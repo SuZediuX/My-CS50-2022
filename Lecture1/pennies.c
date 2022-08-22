f8735c80d7c80f3666993892ed8e5c43c86c0ac9
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-22 15:46:00 +0100

    Add logic

--- Lecture1/pennies.c
+++ Lecture1/pennies.c
@@ -5,4 +5,7 @@ int main(void)
     float dollar;
     printf("Enter dollar amount: ");
     scanf("%f",&dollar);
+
+    int pennies = dollar * 100;
+    printf("\nPennies based on amount you entered: %i\n",pennies);
 }
