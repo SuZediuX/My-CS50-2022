1d41f38e75efdc3422d269c7126aea4471a3a2d4
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-22 12:06:48 +0100

    Return value from function

--- Lecture1/discount.c
+++ Lecture1/discount.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+float calc_discount(float);
+
 int main(void)
 {
     float regular;
-    printf("What is the regular price?");
+    //const float SALE_PERC_OFF = 0.85;
+    printf("What is the regular price?\n");
     scanf("%f",&regular);
     
+    float sale = calc_discount(regular);
+    printf("The final price will be: %f\n", sale);
+
+    return 0;
+}
+
+float calc_discount(float price)
+{
+    const float SALE_PERC_OFF = 0.85;
+    float sale = price * SALE_PERC_OFF;
+
+    return sale;
 }
