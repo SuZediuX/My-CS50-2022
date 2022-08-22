7580cab22779b8b7faaa016234e611c9a143e1bb
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-22 14:44:59 +0100

    Changed return statement

--- Lecture1/discount.c
+++ Lecture1/discount.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
-float calc_discount(float);
+float calc_discount(float, int);
 
 int main(void)
 {
     float regular;
+    int discount;
     //const float SALE_PERC_OFF = 0.85;
     printf("What is the regular price?\n");
     scanf("%f",&regular);
+
+    printf("What is the discount percentage?\n");
+    scanf("%i", &discount);
     
-    float sale = calc_discount(regular);
+    float sale = calc_discount(regular, discount);
     printf("The final price will be: %f\n", sale);
 
     return 0;
 }
 
-float calc_discount(float price)
+float calc_discount(float price, int perc_off)
 {
-    const float SALE_PERC_OFF = 0.85;
-    float sale = price * SALE_PERC_OFF;
+    //const float SALE_PERC_OFF = 1 - perc_off;
+    //float sale = price * SALE_PERC_OFF;
 
-    return sale;
+    return price * (100 - perc_off) / 100;
 }
