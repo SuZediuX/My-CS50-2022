8631e57d010bca86fa5f4606239b923f3af610f6
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-22 14:59:31 +0100

    Add input validation

---
+++ Lecture1/mario.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    do
+    {
+        printf("Enter width: \n");
+        scanf("%i",&n);
+    }
+    while(n < 1);
+
+    for(int iter=0; iter<n; iter++)
+    {
+        printf("?");
+    }
+    printf("\n");
+}
