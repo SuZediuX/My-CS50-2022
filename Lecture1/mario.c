935d8efc94520ce2224e1c12af30c47f57022586
Xeno Zedux <56885709+SuZediuX@users.noreply.github.com>
2022-08-22 15:15:43 +0100

    Replace do/while with while

--- Lecture1/mario.c
+++ Lecture1/mario.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
     int size;
-
-    do
+/*    do
     {
         printf("Enter width: \n");
         scanf("%i",&size);
     }
     while(size < 1);
+*/
+    while(true)
+    {
+        printf("Enter width: \n");
+        scanf("%i",&size);
+        if(size>0)
+        {
+            break;
+        }
+    }
 
     for(int iter=0; iter<size; iter++)
     {
